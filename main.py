2546bd1fdc536fa9b7b8c91a461310b67be9e88a
pawan-25-code <official.pawan2008@gmail.com>
2026-06-15 09:03:51 +0530

    Add number guessing game in main.py
    # Python Number Guessing Game 🎯
    
    A simple, interactive command-line mini-project built using Python. 
    
    ## Features
    - Generates a random number between 1 and 100.
    - Provides real-time feedback (Too High / Too Low).
    - Handles user exit smoothly (Press 'q' or 'Q' to quit).
    
    ## How to Run
    Run the `main.py` file in any Python environment.

---
+++ main.py
@@ -0,0 +1,22 @@
+import random
+
+print("Let's play a game💪:- GUESS NUMBER IN BETWEEN 1 TO 100...")
+target = random.randint(1, 100)
+
+while True:
+    user_choice = input("GUESS THE TARGET OR QUIT(Q):")
+    
+    if (user_choice.upper() == "Q"):
+        break
+    user_choice = int(user_choice)
+    
+    if (user_choice == target):
+        print("__WELDONE__")
+        print("🥳YOU SUCCESSFULLY GUESS THE TARGET!!🎉")
+        break
+    elif (user_choice < target):
+        print("😔YOUR NUMBER IS LOWER THAN TARGET SO, TAKE ANOTHER BIGGER NUMBER!!")
+    else:
+        print("😔YOUR NUMBER IS BIGGER THAN TARGET SO, TAKE ANOTHER SMALLER NUMBER!!")
+
+print("__GAME OVER__")
