69ee9e04c13c6c0a3092de11177aa46d1a4e766f
pawan-25-code <official.pawan2008@gmail.com>
2026-06-21 10:47:05 +0530

    Refactor condition checks in number guessing game

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+"""simple number guessing game"""
 import random
 
 print("Let's play a game💪:- GUESS NUMBER IN BETWEEN 1 TO 100...")
@@ -6,15 +7,15 @@ target = random.randint(1, 100)
 while True:
     user_choice = input("GUESS THE TARGET OR QUIT(Q):")
     
-    if (user_choice.upper() == "Q"):
+    if user_choice.upper() == "Q":
         break
     user_choice = int(user_choice)
     
-    if (user_choice == target):
+    if user_choice == target:
         print("__WELDONE__")
         print("🥳YOU SUCCESSFULLY GUESS THE TARGET!!🎉")
         break
-    elif (user_choice < target):
+    elif user_choice < target:
         print("😔YOUR NUMBER IS LOWER THAN TARGET SO, TAKE ANOTHER BIGGER NUMBER!!")
     else:
         print("😔YOUR NUMBER IS BIGGER THAN TARGET SO, TAKE ANOTHER SMALLER NUMBER!!")
